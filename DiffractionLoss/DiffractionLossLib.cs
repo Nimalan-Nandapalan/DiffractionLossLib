6f0faad30af7559a8c129eb3ba89cffa58389bb0
nima <i.can.haz.programming@gmail.com>
2019-04-14 05:42:42 +1000

    NextLon and NextLat functions implemented courtesy of http://www.gavaghan.org/blog/free-source-code/geodesy-library-vincentys-formula/

---
+++ DiffractionLoss/DiffractionLossLib.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ExcelDna.Integration;
+
+using Gavaghan.Geodesy;
+
+namespace DiffractionLossLib
+{
+    public class DiffractionLoss
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(string name)
+        {
+            MessageBox.Show("Hello " + name);
+
+            return "Hello " + name;
+        }
+
+        [ExcelFunction(Description = "Next Longitude based on Vincenty's Algorithm")]
+        public static double NextLon(double lat, double lon, double az, double distance)
+        {
+            GeodeticCalculator geoCalc = new GeodeticCalculator();
+            Ellipsoid reference = Ellipsoid.WGS84;
+
+            GlobalCoordinates start = new GlobalCoordinates(new Angle(lat), new Angle(lon));
+            Angle azimuth = new Angle(az);
+
+            Angle endBearing;
+
+            GlobalCoordinates dest = geoCalc.CalculateEndingGlobalCoordinates(reference, start, azimuth, distance, out endBearing);
+
+            return dest.Longitude.Degrees;
+        }
+
+        [ExcelFunction(Description = "Next Latitude based on Vincenty's Algorithm")]
+        public static double NextLat(double lat, double lon, double az, double distance)
+        {
+            GeodeticCalculator geoCalc = new GeodeticCalculator();
+            Ellipsoid reference = Ellipsoid.WGS84;
+
+            GlobalCoordinates start = new GlobalCoordinates(new Angle(lat), new Angle(lon));
+            Angle azimuth = new Angle(az);
+
+            Angle endBearing;
+
+            GlobalCoordinates dest = geoCalc.CalculateEndingGlobalCoordinates(reference, start, azimuth, distance, out endBearing);
+
+            return dest.Latitude.Degrees;
+        }
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DiffractionLossLib
 A library to assist with difrraction of loss calculations
+
+Potential things to add: 
+https://andysprague.com/2017/02/03/my-first-custom-excel-ribbon-using-excel-dna/
+https://andysprague.com/2017/07/03/show-message-boxes-with-excel-dna/
