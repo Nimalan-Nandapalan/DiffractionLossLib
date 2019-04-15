9d7c7b9cdc161aff6b9cc51fefcb176f8845111c
nima <i.can.haz.programming@gmail.com>
2019-04-15 22:07:59 +1000

    Azimuth, Distance and an idea to return a result over more than one cell

--- DiffractionLoss/DiffractionLossLib.cs
+++ DiffractionLoss/DiffractionLossLib.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using ExcelDna.Integration;
+using Excel = Microsoft.Office.Interop.Excel;
 
 using Gavaghan.Geodesy;
 using SRTM;
@@ -14,12 +15,80 @@ namespace DiffractionLossLib
 {
     public class DiffractionLoss
     {
+        private GeodeticCalculator geoCalc;
+        private bool recomputeRequired;
+
         [ExcelFunction(Description = "My first .NET function")]
         public static string SayHello(string name)
         {
             MessageBox.Show("Hello " + name);
 
             return "Hello " + name;
+        }
+
+
+        [ExcelFunction(Description = "Sets cell below to 'Potato'",  IsMacroType = true)]
+        public static void Potato( [ExcelArgument(AllowReference = true)] object _arg)
+        {
+            Excel.Application app = (Excel.Application)ExcelDnaUtil.Application;
+            Excel.Range range = app.ActiveCell;
+
+            ExcelReference targetCell;
+
+            targetCell = _arg as ExcelReference;
+            //targetCell = new ExcelReference(range.Row - 1 + 1, range.Column - 1);
+
+            ExcelAsyncUtil.QueueAsMacro( () => { targetCell.SetValue("Potato"); } );
+
+            //return $"=Potato({targetCell.ToString()})";
+            return;
+        }
+               
+        [ExcelFunction(Description = "Array value function", IsMacroType = true))]
+        public static double[,] MakeList()//object _list)
+        {
+            //var list = _list as double[];
+            double[,] list = new double[2,12];
+
+            //for (var i = 0; i < list.Length; i++)
+            for (int i = 0; i < 12; i++)
+            {
+                //list[i] = list[i] * list[i]; 
+                list[0, i] = i;
+                list[1, i] = i * i;
+            }
+
+            var output = XlCall.Excel(XlCall.xlUDF, "Resize", list);
+
+            return list;
+        }
+
+        [ExcelFunction(Description = "Get Azimuth between two points")]
+        public static double GetAzimuth(double txLat, double txLon, double rxLat, double rxLon)
+        {
+            GeodeticCalculator geoCalc = new GeodeticCalculator();
+            Ellipsoid reference = Ellipsoid.WGS84;
+
+            GlobalCoordinates start = new GlobalCoordinates(new Angle(txLat), new Angle(txLon));
+            GlobalCoordinates end = new GlobalCoordinates(new Angle(rxLat), new Angle(rxLon));
+
+            GeodeticCurve path = geoCalc.CalculateGeodeticCurve(reference, start, end);
+
+            return path.Azimuth.Degrees;
+        }
+
+        [ExcelFunction(Description = "Get distance between two points")]
+        public static double GetDistance(double txLat, double txLon, double rxLat, double rxLon)
+        {
+            GeodeticCalculator geoCalc = new GeodeticCalculator();
+            Ellipsoid reference = Ellipsoid.WGS84;
+
+            GlobalCoordinates start = new GlobalCoordinates(new Angle(txLat), new Angle(txLon));
+            GlobalCoordinates end = new GlobalCoordinates(new Angle(rxLat), new Angle(rxLon));
+
+            GeodeticCurve path = geoCalc.CalculateGeodeticCurve(reference, start, end);
+
+            return path.EllipsoidalDistance;
         }
 
         [ExcelFunction(Description = "Next Longitude based on Vincenty's Algorithm")]
